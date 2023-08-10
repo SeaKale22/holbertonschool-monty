8821bb80e06d57acaca022ae7da8df7898e87bba
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-10 11:14:51 -0700

    added new line to add error

--- add_nop.c
+++ add_nop.c
@@ -8,7 +8,7 @@ void op_add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	stack_t *tmp = *stack;
